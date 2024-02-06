31e1ad0351e38fb80209f2aef83949f2d9a7e21e
JCovi <joshpcovington@gmail.com>
2024-02-05 21:07:23 -0700

    some quest routes code

---
+++ server/routes/api/QuestRoutes.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from '../pages/Dashboard';
+import QuestInfo from '../pages/QuestInfo';
+import Quest from '../pages/Quest';
+import NotFound from '../pages/NotFound';
+
+const QuestRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/info" element={<QuestInfo />} />
+      <Route path="/quest" element={<Quest />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
+
+export default QuestRoutes;
